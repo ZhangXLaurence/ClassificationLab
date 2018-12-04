1187963cced3aead30e9e47b0ca59b438180337c
Dukechris <zhangxiao1688@hotmail.com>
2018-12-04 14:26:18 +0800

    Fix

--- bases/Losses/MarginInnerProduct.py
+++ bases/Losses/MarginInnerProduct.py
@@ -97,9 +97,9 @@ class InnerProductWithScaleButNoUse(nn.Module):
         # print('Now average theta is {:.2f}, max theta is {:.2f}, min theta is {:.2f}'.format(avg_theta, max_theta, min_theta))
         ############################## Theta ##############################
         # Calculate logits
-        evaluation_logits = torch.matmul(feat, torch.transpose(self.weights, 0, 1))
-        train_logits = evaluation_logits
-        return evaluation_logits, train_logits
+        valuation_logits = torch.matmul(feat, torch.transpose(self.weights, 0, 1))
+        train_logits = valuation_logits
+        return valuation_logits, train_logits
 
 
 
