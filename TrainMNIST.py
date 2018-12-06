6cbad628081d93588dd94b1e79d4c7197a695154
Dukechris <zhangxiao1688@hotmail.com>
2018-12-06 21:27:42 +0800

    Fix

--- TrainMNIST.py
+++ TrainMNIST.py
@@ -131,9 +131,9 @@ def main():
     arg_classNum = 10
     
     # Learning rate arg
-    arg_BaseLr = 0.005
+    arg_BaseLr = 0.01
     arg_Momentum = 0.5
-    arg_WeightDecay = 0.0005
+    arg_WeightDecay = 0.0000000000000000000005
 
     # Learning rate scheduler
     arg_LrEpochStep = 20
