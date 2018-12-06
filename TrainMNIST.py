0b985de2b6b2d4fa54e68c61b849863140d413e3
Dukechris <zhangxiao1688@hotmail.com>
2018-12-07 00:49:03 +0800

    FIx

--- TrainMNIST.py
+++ TrainMNIST.py
@@ -123,11 +123,11 @@ def main():
 
     # Data arg
     arg_TrainDataPath = './data'
-    arg_TrainBatchSize = 32
+    arg_TrainBatchSize = 128
     arg_TestBatchSize = 1024
 
-    # arg_FeatureDim = 2
-    arg_FeatureDim = 32
+    arg_FeatureDim = 2
+    # arg_FeatureDim = 32
     arg_classNum = 10
     
     # Learning rate arg
