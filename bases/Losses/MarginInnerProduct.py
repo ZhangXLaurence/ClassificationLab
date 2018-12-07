2ea5d8b07f762d185f251853acf7e897a0a1f9d7
Dukechris <zhangxiao1688@hotmail.com>
2018-12-07 14:52:07 +0800

    Fix

--- bases/Losses/MarginInnerProduct.py
+++ bases/Losses/MarginInnerProduct.py
@@ -178,6 +178,8 @@ class MetricLogits(nn.Module):
 
         max_stdmetric = torch.max(std_metric).item()
         min_stdmetric = torch.min(std_metric).item()
+
+        std_metric = (std_metric - min_stdmetric) / (max_stdmetric - min_stdmetric)
         # print('Now average pos. dist. and all avg. are {:.4f} and {:.4f}'.format(avg_distance, metric_mean))
         # print('Now max stdm. and min stdm. are {:.4f} and {:.4f}'.format(max_stdmetric, min_stdmetric))
 
@@ -185,7 +187,7 @@ class MetricLogits(nn.Module):
 
         valuation_logits = -1.0 * metric
         # train_logits = 100 * std_metric
-        train_logits = 10.0 * (10.0 - std_metric)
+        train_logits = -10.0 * std_metric)
         return valuation_logits, train_logits
 
 
