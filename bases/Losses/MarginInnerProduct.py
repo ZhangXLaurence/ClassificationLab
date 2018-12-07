0929dca325dcaf15dd47d558f164eace285d7a87
Dukechris <zhangxiao1688@hotmail.com>
2018-12-07 23:26:27 +0800

    Fix

--- bases/Losses/MarginInnerProduct.py
+++ bases/Losses/MarginInnerProduct.py
@@ -110,7 +110,7 @@ class MetricLogits(nn.Module):
         self.class_num = class_num
         self.weights = nn.Parameter( torch.FloatTensor(class_num, feature_dim))
         # nn.init.xavier_uniform_(self.weights)
-        nn.init.uniform_(self.weights, a=-10.0, b=10.0)
+        nn.init.uniform_(self.weights, a=-5.0, b=5.0)
         # self.weights.requires_grad = False
         # self.weights = nn.Parameter(torch.rand(class_num, feature_dim))
 
